429e9b8ca265501803fbe2e0953c5b2da5da8e8f
cheryl-pope <cheryl.pope@adelaide.edu.au>
2021-09-09 15:04:53 +0930

    test first err1.c file

--- Prac-6/compile.sh
+++ Prac-6/compile.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 # Prepare submission
-cp /autograder/submission/fib.c /autograder/source/fib.c
+cp /autograder/submission/week8practice/*.c /autograder/source/
 
 cd /autograder/source
 
-make fib
+gcc gcc -Wall -Werror=vla -std=c11 -o err1 err1.c
+gcc gcc -Wall -Werror=vla -std=c11 -o err2 err2.c
+gcc gcc -Wall -Werror=vla -std=c11 -o err3 err3.c
+gcc gcc -Wall -Werror=vla -std=c11 -o form_letter form_letter.c
+gcc gcc -Wall -Werror=vla -std=c11 -o digits digits.c
+gcc gcc -Wall -Werror=vla -std=c11 -o education education.c
+gcc gcc -Wall -Werror=vla -std=c11 -o months months.c
