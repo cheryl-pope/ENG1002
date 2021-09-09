4e326bca447595bab8536e8c09153be6fc77518c
cheryl-pope <cheryl.pope@adelaide.edu.au>
2021-09-09 14:36:06 +0930

    initialise

--- Prac-6/setup.sh
+++ Prac-6/setup.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y python3 python3-pip python3-dev
-apt-get install -y g++
-apt-get install -y jsoncpp
+apt-get install -y python python-pip python-dev
 
-apt-get install -y libjsoncpp-dev
-
-apt-get install -y clang
-
-pip3 install -r /autograder/source/requirements.txt
-
-ln -s /usr/bing/clang++ /usr/bin/clang++
+pip install -r /autograder/source/requirements.txt
