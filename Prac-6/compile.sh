1310521e9dd5bac62ffae1882334ee7ffd6dcba1
cheryl-pope <cheryl.pope@adelaide.edu.au>
2021-09-10 17:26:41 +0930

    first full integration of one test

--- Prac-6/compile.sh
+++ Prac-6/compile.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 # Prepare submission
-cp /autograder/submission/week8practice/*.c /autograder/source/
+cp /autograder/submission/*.c /autograder/source/
 
 cd /autograder/source
 
-gcc gcc -Wall -Werror=vla -std=c11 -o err1 err1.c
-gcc gcc -Wall -Werror=vla -std=c11 -o err2 err2.c
-gcc gcc -Wall -Werror=vla -std=c11 -o err3 err3.c
-gcc gcc -Wall -Werror=vla -std=c11 -o form_letter form_letter.c
-gcc gcc -Wall -Werror=vla -std=c11 -o digits digits.c
-gcc gcc -Wall -Werror=vla -std=c11 -o education education.c
-gcc gcc -Wall -Werror=vla -std=c11 -o months months.c
+gcc -Wall -Werror=vla -std=c11 -o err1 err1.c
+gcc -Wall -Werror=vla -std=c11 -o err2 err2.c
+gcc -Wall -Werror=vla -std=c11 -o err3 err3.c
+gcc -Wall -Werror=vla -std=c11 -o form_letter form_letter.c
+gcc -Wall -Werror=vla -std=c11 -o digits digits.c
+gcc -Wall -Werror=vla -std=c11 -o education education.c
+gcc -Wall -Werror=vla -std=c11 -o months months.c
