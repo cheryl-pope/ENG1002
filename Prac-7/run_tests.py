dd999e5c74d7fc693909bc32396b069bd6431904
cheryl-pope <cheryl.pope@adelaide.edu.au>
2021-10-06 17:28:20 +1030

    updates

--- Prac-7/run_tests.py
+++ Prac-7/run_tests.py
@@ -1,6 +1,13 @@
 import unittest
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
+from gradescope_utils.autograder_utils.files import check_submitted_files
 from test_generator import find_data_directories, build_test_class, TestMetaclass
+
+missing_files = check_submitted_files(['odds_evens.c', 'process_ages.c', 'to_uppercase.c', 'to_uppercase2.c', 'reverse_in_place.c'])
+for path in missing_files:
+    print('Missing {0}'.format(path))
+self.assertEqual(len(missing_files), 0, 'Missing some required files!')
+print('All required files submitted!')
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
