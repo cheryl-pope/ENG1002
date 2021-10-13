51c3db7427da61fe98d36629ea038c2472d894f0
cheryl-pope <cheryl.pope@adelaide.edu.au>
2021-10-13 18:40:40 +1030

    test directory traversal

--- stderr-test/run_tests.py
+++ stderr-test/run_tests.py
@@ -4,10 +4,18 @@ from test_generator import find_data_directories, build_test_class, TestMetaclas
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
+    parent_suite=suite
+    # check for suites
+    for root, dirs, files in os.walk('./test_data', topdown=true):
+        new_suite = unittest.TestSuite();
+        parent_suite.addTest(new_suite);
 
-    for name in find_data_directories():
-        klass = build_test_class(name)
-        suite.addTest(klass(TestMetaclass.test_name(name)))
+        # if we're in a bottom level
+        if files:
+            for name in files():
+                klass = build_test_class(name)
+                new_suite.addTest(klass(TestMetaclass.test_name(name)))
+        parent_suite = new_suite
 
     with open('/autograder/results/results.json', 'w') as f:
         JSONTestRunner(visibility='visible', stream=f).run(suite)
